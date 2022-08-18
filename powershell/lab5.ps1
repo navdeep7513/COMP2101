11049ff49feb6ac4ea6d555ee0582a3d908dbf36
navdeep7513 <106150207+navdeep7513@users.noreply.github.com>
2022-08-17 22:24:19 -0400

    Lab 5

---
+++ powershell/lab5.ps1
@@ -0,0 +1,32 @@
+﻿param (
+[switch]$System
+,[switch]$Disks
+,[switch]$Networks
+)
+
+if ($System)
+{
+	HardwareInformation
+	OperatingSystemInformation
+	ProcessorInformation
+	PhysicalMemoryInformation
+	VideoCardInformation
+}
+elseif ($Disks)
+{
+	DiskinfoInformation
+}
+elseif($Networks)
+{
+	NetworkInformation
+}
+else
+{
+	HardwareInformation
+	OperatingSystemInformation
+	ProcessorInformation
+	PhysicalMemoryInformation
+	DiskinfoInformation
+	NetworkInformation
+	VideoCardInformation
+}
